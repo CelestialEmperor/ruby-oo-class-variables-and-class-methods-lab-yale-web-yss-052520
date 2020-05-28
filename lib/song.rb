343dc566b772131c3575fdd406b6cfeb2d10ac36
Jack Ma <mapeiyao@gmail.com>
2020-05-28 21:15:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,9 +26,9 @@ class Song
     artist_count = {}
     @@artists.each do |artist|
       if artist_count[artist]
-        artist_count += 1
+        artist_count[artist] += 1
       else
-        artist_count = 1
+        artist_count[artist] = 1
       end
     end
     artist_count
@@ -40,11 +40,11 @@ class Song
   
   def self.genre_count
     genre_count = {}
-    @@genre.each do |genre|
+    @@genres.each do |genre|
       if genre_count[genre]
-        genre_count += 1
+        genre_count[genre] += 1
       else
-        genre_count = 1
+        genre_count[genre] = 1
       end
     end
     genre_count
