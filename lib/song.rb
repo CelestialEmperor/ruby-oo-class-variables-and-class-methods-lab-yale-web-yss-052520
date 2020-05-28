3628400d7b199d8759839b13da1e7b1309c2bc8d
Jack Ma <mapeiyao@gmail.com>
2020-05-28 21:01:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,14 +5,19 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0
+  @@genres = []
   
   def initialize
     @@count += 1
+    @@genres << self
   end
   
   def self.count
-    puts 
+    puts @@count
+  end
   
+  def self.genres
+    @@genres.
   
 end
 
