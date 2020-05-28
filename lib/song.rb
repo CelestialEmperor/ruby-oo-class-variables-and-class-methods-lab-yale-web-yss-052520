9060863970c5ac4a69563507c5cef6e6482d41ce
Jack Ma <mapeiyao@gmail.com>
2020-05-28 21:05:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,15 +9,12 @@ class Song
   @@artists = []
   
   def initialize(name, artist, genre)
-    
     @name = name
     @artist = artist
     @genre = genre
-    
     @@count += 1
-    @@genres << self
-    @@artists << self
-    
+    @@genres << genres
+    @@artists << artists
   end
   
   def self.count
