4565ec2ab2771adef04dfbdfa1d89897fda9ed56
Jack Ma <mapeiyao@gmail.com>
2020-05-28 21:10:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 class Song
   
   attr_accessor :name, :artist, :genre
@@ -25,18 +23,30 @@ class Song
     puts @@genres.uniq
   end
   
+  def self.genre_count
+    genre_count = {}
+    @@genre.each do |genre|
+      if genre_count[genre]
+        genre_count += 1
+      else
+        genre_count = 1
+      end
+    end
+  end
+  
   def self.artists
     puts @@artists.uniq
   end
   
-  def self.genre_count
-    genre_count = {}
-    @@genre.each do |genre|
-      if genre_count 
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count += 1
+      else
+        artist_count = 1
+      end
+    end
   end
   
-  
 end
-
-binding.pry
-0
